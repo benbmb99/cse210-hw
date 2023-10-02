0498ec2f912cbd11108bf7d7154972fc8023b047
Benjamin Bown <benbmb99@gmail.com>
2023-10-02 16:27:19 -0600

    Completed Prep 4 Assignment.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment mine = new Assignment("Josh Kyleson", "Derivatives");
+        Console.WriteLine(mine.GetSummary());
+        MathAssignment yours = new MathAssignment("Kyle Josheson", "Fractals", "4.5", "1-18");
+        Console.WriteLine(yours.GetSummary());
+        Console.WriteLine(yours.GetHomeworkList());
+        WritingAssignment ours = new WritingAssignment("Kyle's Son, Josh", "Environment", "How I can stop the environment from being evil?");
+        Console.WriteLine(ours.GetSummary());
+        Console.WriteLine(ours.GetWritingInformation());
     }
 }
