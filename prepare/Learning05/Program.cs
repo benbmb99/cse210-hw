e64133058f247beebbb2d8bbef222c32ebccb3c1
Benjamin Bown <benbmb99@gmail.com>
2023-10-05 10:08:52 -0600

    Completed Week 5 Learning activities.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> list = new();
+        Square s = new("yellow", 5);
+        list.Add(s);
+        //Console.WriteLine(s.GetColor());
+        //Console.WriteLine(s.GetArea());
+        Rectangle r = new("red", 3, 5);
+        list.Add(r);
+        //Console.WriteLine(r.GetColor());
+        //Console.WriteLine(r.GetArea());
+        Circle c = new("blue", 4);
+        list.Add(c);
+        //Console.WriteLine(c.GetColor());
+        //Console.WriteLine(c.GetArea());
+
+        foreach(Shape q in list){
+            Console.WriteLine(q.GetColor());
+            Console.WriteLine(q.GetArea());
+        }
     }
 }
